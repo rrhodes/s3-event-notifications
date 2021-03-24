5de13d4bfc43c9f89d0f5747115277e00f041594
Ross Rhodes <ross.rhodes@postcodelottery.co.uk>
2021-03-24 19:25:42 +0000

    CDK infrastructure for S3 event notification

--- bin/s3-event-notifications.ts
+++ bin/s3-event-notifications.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import { App } from '@aws-cdk/core';
 import { S3EventNotificationsStack } from '../lib/s3-event-notifications-stack';
 
-const app = new cdk.App();
+const app = new App();
 new S3EventNotificationsStack(app, 'S3EventNotificationsStack');
--- lib/s3-event-notifications-stack.ts
+++ lib/s3-event-notifications-stack.ts
@@ -1,9 +1,41 @@
-import * as cdk from '@aws-cdk/core';
+import { Code, Function as LambdaFunction, Runtime } from '@aws-cdk/aws-lambda';
+import { BlockPublicAccess, Bucket, BucketEncryption, EventType } from '@aws-cdk/aws-s3';
+import { LambdaDestination } from '@aws-cdk/aws-s3-notifications';
+import { Construct, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
 
-export class S3EventNotificationsStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class S3EventNotificationsStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'Bucket', {
+      autoDeleteObjects: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      bucketKeyEnabled: true,
+      encryption: BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    this.lambdaWithS3EventNotification(this, 'LambdaOne', bucket, 'subfolder-one');
+  }
+
+  private lambdaWithS3EventNotification(
+    scope: Construct,
+    lambdaId: string,
+    bucket: Bucket,
+    eventNotificationPrefixSubfolder: string
+  ) {
+    const lambdaHandler = 'hello_world';
+    const lambda = new LambdaFunction(scope, lambdaId, {
+      code: Code.fromInline(`def ${lambdaHandler}(): print('Hello from Lambda one!')`),
+      handler: lambdaHandler,
+      runtime: Runtime.PYTHON_3_8,
+    });
+    
+    bucket.addEventNotification(
+      EventType.OBJECT_CREATED,
+      new LambdaDestination(lambda),
+      { prefix: `trigger-folder/${eventNotificationPrefixSubfolder}` },
+    )
   }
 }
